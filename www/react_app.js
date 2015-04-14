ef6bc238bf88962cb7b48423d4e7aa6950611ada
Nick Clarke <nickolasclarke@gmail.com>
2015-04-14 04:12:46 +0000

    added event handling

--- www/react_app.js
+++ www/react_app.js
@@ -16,10 +16,17 @@ var PhoneForm = React.createClass({
       countryPlaceholder: "Where are you going?"
     } )
   },
-  handleSubmit: function(e){
-    e.preventDefault();
-    console.log('HERE');
-    return;
+  handleSubmit: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
   },
   render: function(){
     return (
@@ -92,4 +99,4 @@ var WanderApp = React.createClass({
   }
 });
 
-React.render(<WanderApp data={data}/>, document.getElementById('app'));
+React.render(<WanderApp url="http://myapiurl.com/reports/"/>, document.getElementById('app'));
