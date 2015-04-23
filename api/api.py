22d00a882d5b46e24171ba05279ff7654928c8cf
Nick Clarke <nickolasclarke@gmail.com>
2015-04-23 04:16:50 +0000

    added functionality for working api on c9.io

--- api/api.py
+++ api/api.py
@@ -1,4 +1,5 @@
 import json
+import os
 from flask import Flask
 from flask import render_template
 from flask import jsonify
@@ -8,31 +9,33 @@ ROOT = 'json'
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET'])
 def get_root():
-  paths = ['/phones','/networks','/countries','/reports']
-  return render_template('root.html', paths=paths)
+  api_paths = ['/phones','/networks','/countries','/reports']
+  return render_template('root.html', paths=api_paths)
     
-@app.route('/phones')
+@app.route('/phones',methods=['GET'])
 def get_phones():
   js_data = read_json(ROOT + '/phones.json')
+  #js_data['data'].append({'foo':'bar'}) #<== Example of how we can add data to our base json objects
   return jsonify(js_data)
 
-@app.route('/networks')
+@app.route('/networks',methods=['GET'])
 def get_networks():
   js_data = read_json(ROOT + '/networks.json')
   return jsonify(js_data)
 
-@app.route('/countries')
+@app.route('/countries',methods=['GET'])
 def get_countries():
   js_data = read_json(ROOT + '/countries.json')
   return jsonify(js_data)
   
-@app.route('/reports')
+@app.route('/reports',methods=['GET'])
 def get_reports():
   js_data = read_json(ROOT + '/reports.json')
   return jsonify(js_data)
   
+# Helper method to read the flat json
 def read_json(file_path):
   json_data = open(file_path, 'r')
   data = json.load(json_data)
@@ -40,4 +43,8 @@ def read_json(file_path):
   return data
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  port = int(os.environ['PORT'])
+  host = os.environ['IP']
+  app.run(debug=True,port=port,host=host)
+
+
